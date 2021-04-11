101dd6cb3e78f6f705f601976c808d1aef154c2b
Mir Nawaz <inbox.mirnawaz@gmail.com>
2021-04-11 23:20:36 +0200

    authentication

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-clone 
-composer install
-symfony server:start
+Symfony App
+--------------
+```bash
+$ composer install
+$ symfony server:start
+```
+
+Migrations 
+--------------
+```bash 
+$ php bin/console doctrine:migrations:diff
+$ php bin/console doctrine:schema:validate
+$ bin/console doctrine:migration:migrate -n
+```
---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/registration", name="registration")
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @return Response
+     */
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('username')
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password'],
+            ])
+            ->add('register', SubmitType::class, [
+                'attr' => [ 'class' => 'btn btn-success float-right' ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $data = $form->getData();
+            $user = new User();
+            $user->setUsername($data['username']);
+            $user->setPassword(
+                $passwordEncoder->encodePassword($user, $data['password'])
+            );
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
